f391711e63056c5adecb75023c5eba041e060203
Chris Andrews <chris@nodnol.org>
2012-02-25 11:07:54 +0000

    Quick hack build fixes to allow wfx to be built on OSX.
     * Take libfftw from MacPorts in /opt/local
     * Replace clock_gettime calls with clock_get_time from mach/clock.h
    
    This allows the build of wfx to complete, which will play back recorded
    ensembles from a Linux host with the wavefinder.ko driver.

--- src/wfafc.c
+++ src/wfafc.c
@@ -23,6 +23,11 @@
 */
 #include "opendab.h"
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
 extern int wf_mem_write(int, unsigned short, unsigned short);
 
 int wf_afc(int fd, double afcv)
@@ -35,7 +40,19 @@ int wf_afc(int fd, double afcv)
 	short afc_val;
 	struct timespec tp;
 
-	clock_gettime(CLOCK_REALTIME, &tp);
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+clock_serv_t cclock;
+mach_timespec_t mts;
+host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+clock_get_time(cclock, &mts);
+mach_port_deallocate(mach_task_self(), cclock);
+tp.tv_sec = mts.tv_sec;
+tp.tv_nsec = mts.tv_nsec;
+
+#else
+clock_gettime(CLOCK_REALTIME, &tp);
+#endif
+
 	tp.tv_sec = tp.tv_sec % 1000000;
 	cmsec = tp.tv_sec * 1000 + tp.tv_nsec/1000000;
 	dt = cmsec - lmsec;
