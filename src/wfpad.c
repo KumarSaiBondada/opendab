4493de0d766deb1468597a1c755270ea7ef33a90
Chris Andrews <chris@nodnol.org>
2016-04-15 22:40:26 +0100

    Remove commented debug

--- src/wfpad.c
+++ src/wfpad.c
@@ -11,7 +11,7 @@ struct pad_state *init_pad(void)
         pad->bitrate = 0;
         pad->sampling_freq = 0;
         pad->ptr = pad->segment;
-        
+
         return pad;
 }
 
@@ -37,26 +37,22 @@ void wfpad(struct pad_state *pad, unsigned char *buf, int bytes)
 
         fpad = spack(&buf[bytes-2]);
         memcpy(&p, &fpad, 2);
-        //fprintf(stderr, "scf_words: %d FType: %d CIFlag: %d Z: %d\n", scf_words, p.FType, p.CIFlag, p.Z);
 
         if (p.FType == 0) {
                 memcpy(&p00, &buf[bytes-2], 1);
 
-                //fprintf(stderr, " FType: %d XPadInd: %d ByteLInd: %d\n", p00.FType, p00.XPadInd, p00.ByteLInd);
-
                 if (p00.XPadInd == 1) {
                         /* offset in buf of the start of the x-pad data */
                         int xpadoff = bytes - (1 + scf_words + 2);
-                        
+
                         if (p.CIFlag == 1) {
                                 pad->ci = buf[xpadoff];
 
                                 if (pad->ci == 2) {
                                         short prefix = spack(&buf[xpadoff - 1]);
                                         memcpy(&dls, &prefix, 2);
-                                        
-                                        //fprintf(stderr, "   DLS: f1: %d f2: %d cmd: %d first: %d toggle: %d\n", dls.f1, dls.f2, dls.cmd, dls.first, dls.toggle);
-                                        
+
+
                                         if (dls.first == 2) { // first segment
                                                 if (dls.toggle != pad->toggle) {
                                                         pad->toggle = dls.toggle;
@@ -65,7 +61,7 @@ void wfpad(struct pad_state *pad, unsigned char *buf, int bytes)
 
                                                 /* reset "next byte" pointer */
                                                 pad->ptr = pad->segment;
-                                                
+
                                                 /* reset label length */
                                                 pad->dls_length = 0;
                                         }
@@ -90,8 +86,6 @@ void wfpad(struct pad_state *pad, unsigned char *buf, int bytes)
                                 }
                         }
                         else {
-                                //fprintf(stderr, "   cont: CI: %d first: %d seglen: %d left: %d dls_length: %d\n", pad->ci, pad->first, pad->seglen, pad->left, pad->dls_length);
-
                                 if (pad->ci == 2) {
                                         for (int i = 0; i < 4; i++) {
                                                 if (pad->left > 2) {
@@ -109,21 +103,13 @@ void wfpad(struct pad_state *pad, unsigned char *buf, int bytes)
                                         }
                                         /* if we've got the last character of any segment, do a crc check */
                                         if (pad->left == 0) {
-                                                //unsigned short crc = spack(pad->crc);
-                                                //fprintf(stderr, "crc should be: %x\n", crc);
-
                                                 crc16check(pad->segment, pad->seglen);
-
-                                                //for (int i = 0; i < pad->seglen; i++)
-                                                //       fprintf(stderr, "0x%02x ", pad->segment[i]);
-                                                //fprintf(stderr, "\n");
-
                                                 pad->segment[pad->seglen] = '\0';
 
                                                 /* assume crc ok; should check */
                                                 /* copy segment's text into the label */
                                                 memcpy(pad->label + (pad->dls_length - (pad->seglen - 2)), pad->segment + 2, pad->seglen - 2);
-                                                
+
                                                 /* if we've got the last character of a last DLS segment, display it */
                                                 if (pad->first == 1) { // last segment
                                                         pad->label[pad->dls_length] = '\0';
